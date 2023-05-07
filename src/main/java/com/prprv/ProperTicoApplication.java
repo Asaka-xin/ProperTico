4d8964722ac46df1c63517b5d5dee414177cd0fc
Christine <Ty.christine@outlook.com>
2023-05-07 19:49:16 +0800

    Initiation.

---
+++ src/main/java/com/prprv/ProperTicoApplication.java
@@ -0,0 +1,13 @@
+package com.prprv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProperTicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProperTicoApplication.class, args);
+    }
+
+}
