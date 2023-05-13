459a41e91b505e69a13bd977f59c88729cc70d78
phj233 <2780990934@qq.com>
2023-05-13 15:36:24 +0800

    :sparkles: feat(MaintenancerequestsService): Implements some interfaces of MaintenancerequestsService

--- src/main/java/com/prprv/service/MaintenancerequestsService.java
+++ src/main/java/com/prprv/service/MaintenancerequestsService.java
@@ -8,34 +8,51 @@ import com.prprv.entity.Maintenancerequests;
  * @author Christine
  * @version 1.0
  * @since 2023/5/11
+ * @see MaintenancerequestsServiceImpl
  */
 public interface MaintenancerequestsService {
 
     /**
      * 创建报修记录
+     *
      * @param maintenancerequests 报修维护信息
      * @return Result
      */
     Result<Object> createMaintenancerequests(Maintenancerequests maintenancerequests);
 
     /**
-     * 处理报修记录
-     * @param maintenancerequests 报修维护信息
+     * 更新报修状态
+     *
      * @return Result
      */
     Result<Object> updateMaintenancerequests(Maintenancerequests maintenancerequests);
 
     /**
      * 查询报修记录
-     * @param maintenancerequests 报修维护信息
+     *
      * @return Result
      */
-    Result<Object> getMaintenancerequests(Maintenancerequests maintenancerequests);
+    Result<Object> getMaintenancerequests();
 
     /**
-     * 删除报修记录
-     * @param maintenancerequests 报修维护信息
+     * 分页查询报修记录
+     *
      * @return Result
      */
-    Result<Object> deleteMaintenancerequests(Maintenancerequests maintenancerequests);
+    Result<Object> getAllMaintenancerequestsByPage(Integer page, Integer size);
+
+    /**
+     * 根据工单编号查询报修记录
+     *
+     * @param id 工单编号
+     * @return Result
+     */
+    Result<Object> getMaintenancerequestsById(Long id);
+    /**
+     * 删除报修记录
+     *
+     * @param id 工单编号
+     * @return Result
+     */
+    Result<Object> deleteMaintenancerequestsById(Long id);
 }
