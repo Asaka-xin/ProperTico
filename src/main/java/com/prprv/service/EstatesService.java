3881c5267147258e9281783f125474e23eb5e28b
phj233 <2780990934@qq.com>
2023-05-13 15:36:08 +0800

    :sparkles: feat(EstatesService): Implements some interfaces of EstatesService

--- src/main/java/com/prprv/service/EstatesService.java
+++ src/main/java/com/prprv/service/EstatesService.java
@@ -7,7 +7,8 @@ import com.prprv.entity.Estates;
  * 小区服务管理接口
  * @author Christine
  * @since 2023/5/11
- * @see Estates
+ * @see Estates 小区实体类
+ * @see EstatesServiceImpl 小区服务实现类
  */
 public interface EstatesService {
 
@@ -27,16 +28,29 @@ public interface EstatesService {
 
     /**
      * 删除小区信息
-     * @param estates 小区信息
+     * @param estatesId 小区id
      * @return Result
      */
-    Result<Object> deleteEstates(Estates estates);
+    Result<Object> deleteEstatesById(Long estatesId);
 
     /**
      * 小区信息展示
-     * @param estates 小区信息
      * @return Result
      */
-    Result<Object> selectEstates(Estates estates);
+    Result<Object> selectEstates();
 
+    /**
+     * 分页查询小区信息
+     * @param page 页码
+     * @param size 每页数量
+     * @return Result
+     */
+    Result<Object> selectAllEstatesByPage(Integer page, Integer size);
+
+    /**
+     * 根据小区id查询小区信息
+     * @param estatesId 小区id
+     * @return Result
+     */
+    Result<Object> selectEstatesById(Long estatesId);
 }
